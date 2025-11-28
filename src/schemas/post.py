c38188b752bcb0165d8e8d01c1916c6a17d8d949
Alessandra Chioquetta <alessandra.chioquetta@gmail.com>
2025-11-28 16:32:22 -0300

    Update timestamp

--- src/schemas/post.py
+++ src/schemas/post.py
@@ -6,12 +6,12 @@ from pydantic import AwareDatetime, BaseModel
 class PostIn(BaseModel):
     title: str
     content: str
-    published_at: str | None = None
+    published_at: datetime | None = None
     published: bool = False
 
 
 class PostUpdateIn(BaseModel):
     title: str | None = None
     content: str | None = None
-    published_at: str | None = None
+    published_at: datetime | None = None
     published: bool | None = None
