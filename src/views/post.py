7723fb71ba3538be2d35f3eeacd34fcdd1f75d56
Alessandra Chioquetta <alessandra.chioquetta@gmail.com>
2025-11-28 17:08:40 -0300

    Update timestamp

--- src/views/post.py
+++ src/views/post.py
@@ -1,10 +1,10 @@
 # from datetime import datetime
 
-from pydantic import BaseModel, NaiveDatetime
+from pydantic import AwareDatetime, BaseModel
 
 
 class PostOut(BaseModel):
     id: int
     title: str
     content: str
-    published_at: NaiveDatetime | None
+    published_at: AwareDatetime | None
